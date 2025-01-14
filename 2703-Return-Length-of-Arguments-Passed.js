9130aba095c397aad698ac8b1eb5cd761ed49a24
Joana Brito <140211258+joanaBrit@users.noreply.github.com>
2025-01-14 14:49:30 +0000

    [2703] [Time Beats: 35.51%] [Memory Beats: 71.87%] - LeetPush

---
+++ 2703-Return-Length-of-Arguments-Passed.js
@@ -0,0 +1,16 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+    let total = args.length
+return total
+};
+
+
+// count how many arguments was passed
+// return the total
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
