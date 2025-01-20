981b5c1384f11224ea349d9d2df517841042968c
Joana Brito <140211258+joanaBrit@users.noreply.github.com>
2025-01-20 15:35:05 +0000

    [2715] [Time Beats: 6.50%] [Memory Beats: 47.22%] - LeetPush

---
+++ 2715-Timeout-Cancellation.js
@@ -0,0 +1,49 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+    const timer = setTimeout(() => {
+        fn(...args)
+    }, t)
+
+    return function cancelFn(){
+        clearTimeout(timer)
+    }
+};
+
+/**
+
+Function fn should be delayed by t milliseconds before being executed.
+Schedule the execution of fn using setTimeout.
+Use the spread operator (...args) to pass the arguments to fn when it is invoked.
+Return a cancel function (cancelFn) that allows clearing the scheduled execution.
+Inside the cancel function, use clearTimeout to stop the execution of fn.
+
+ */
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
